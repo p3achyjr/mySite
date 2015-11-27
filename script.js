8e9c37a92aedaf8f5b45ab39596c037886fc6f4c
System Administrator <root@MacBook-Pro.home>
2015-11-27 01:05:57 -0500

    stuff

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var currPage = "about";
 var currWrapper = "tech";
-var projs = ["pianoscribe", "smartcan", "sonnetify", "heattweet",
+var projs = ["pianoscribe", "repman", "smartcan", "sonnetify", "heattweet",
              "rose", "nujabes", "hair", "ff", "journey"];
 var fadedIn = false;
 var fadedOut = true;
 
+var currId = "";
+var projectsFadedIn = false;
+
 function changePage(currElem) {
-  $("#item-about, #item-projects, #item-resume").removeClass("active");
+  $("#item-about, #item-projects, #item-resume, #item-contact")
+    .removeClass("active");
   currElem.addClass("active");
   $(".container").addClass("hidden");
   var currContainer = currElem.text().toLowerCase();
@@ -23,7 +27,8 @@ function changeProjectWrapper(currProjectWrapper) {
 }
 
 function bindMainNavElems() {
-  $("#item-about, #item-projects, #item-resume").click( function() {
+  $("#item-about, #item-projects, #item-resume, #item-contact")
+  .click( function() {
     var thisPage = $(this).text();
     if(currPage !== thisPage) {
       currPage = thisPage;
@@ -64,7 +69,7 @@ function onHoverIn(i) {
     var currOverlay = "#proj-".concat(projs[i]).concat("-overlay");
     $(currOverlay).removeClass("hidden");
     $(currOverlay).stop(true, false).fadeTo(300, 1);
-    $(currOverlay).fadeTo(300, 1);
+    // $(currOverlay).fadeTo(300, 1);
     // $(".thumb").stop(true, false).fadeTo(300, .6);
     // $(".thumb").fadeTo(300, .6);
   }
@@ -72,21 +77,30 @@ function onHoverIn(i) {
 
 function onHoverOut(i) {
   return function() {
-    $(".thumb").each(function() {
-      // fadeBack($(this), projs[i]);
-      var currOverlay = "proj-".concat(projs[i]).concat("-overlay");
-      $(currOverlay).stop(true, false).fadeTo(250, 0);
-      // $(currOverlay).fadeTo(250, 0);
+    // fadeBack($(this), projs[i]);
+    console.log("triggered");
+    var currOverlay = "#proj-".concat(projs[i]).concat("-overlay");
+    $(currOverlay).stop(true, false);
+    $(currOverlay).fadeTo(250, 0, function() {
       $(currOverlay).addClass("hidden");
     });
   }
 }
 
+function fadeInProjDetails(id) {
+  $(id).removeClass("hidden").fadeTo(300, 1);
+}
+
 function onProjClick(i) {
   return function() {
-    $(".page-overlay").removeClass("hidden").fadeTo(400, 1);
     var id = "#proj-".concat(projs[i]).concat("-details");
-    $(".proj-details").removeClass("hidden").attr("id", id);
+    currId = id;
+    console.log(currId);
+    $(".page-overlay").removeClass("hidden").fadeTo(300, 1, function() {
+      setTimeout( function() {
+        fadeInProjDetails(id);
+      }, 200);
+    });
   }
 }
 
@@ -102,11 +116,54 @@ function bindThumbsOnHover() {
   }
 }
 
+function bindCloseBtn() {
+  $(".proj-details").hover(
+    function() {
+      $(".close-btn").stop(true, false).removeClass("hidden").fadeTo(250, 1);
+    }, function() {
+      $(".close-btn").stop(true, false).addClass("hidden").fadeTo(250, 0);
+  });
+  $(".close-btn").click( function() {
+    console.log(currId);
+    $(currId).stop(true, false).fadeTo(200, 0, function() {
+      $(currId).addClass("hidden");
+    });
+    $(".page-overlay").stop(true, false).fadeTo(250, 0, function() {
+      $(".page-overlay").addClass("hidden");
+    });
+  });
+  $(".page-overlay").click( function() {
+    console.log(currId);
+    $(currId).stop(true, false).fadeTo(200, 0, function() {
+      $(currId).addClass("hidden");
+    });
+    $(".page-overlay").stop(true, false).fadeTo(250, 0, function() {
+      $(".page-overlay").addClass("hidden");
+    });
+  })
+}
+
+function fadeInElementsForDramaticEffect() {
+  // feel so pretentious man
+  setTimeout( function() {
+    $(".navbar").fadeTo(800, 1, function() {
+      setTimeout( function() {
+        $("#main-pic").fadeTo(1800, 1, function() {
+          $(".main-content").fadeTo(800, 1);
+        });
+      }, 300);
+    });
+  }, 750);
+}
+
 $(document).ready( function() {
+  window.onload = fadeInElementsForDramaticEffect;
+  fadeInElementsForDramaticEffect();
   bindMainNavElems();
   bindProjectNavElems();
   resizeThumbs();
   bindThumbsOnHover();
+  bindCloseBtn();
 });
 
 
